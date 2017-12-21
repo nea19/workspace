8678913d08cc393542d3d58186a4fb62df740856
云 <752481439@qq.com>
2017-12-21 15:50:13 +0800

    react

--- README.txt
+++ README.txt
@@ -2,4 +2,4 @@ YUN
 2017.12
 
 JQUERY
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js
--- WEBPACK/webpack.config.js
+++ WEBPACK/webpack.config.js
@@ -1,3 +1,9 @@
+// 简书
+// http://www.jianshu.com/p/42e11515c10f
+// 启动
+// npm run server
+
+
 // module.exports = {
 //     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
 //     output: {
@@ -30,7 +36,44 @@
   // 添加baable后
   // npm一次性安装多个依赖模块，模块之间用空格隔开
   //npm install --save-dev babel-core babel-loader babel-preset-env babel-preset-react
-  module.exports = {
+//   module.exports = {
+//     entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+//     output: {
+//         path: __dirname + "/public",//打包后的文件存放的地方
+//         filename: "bundle.js"//打包后输出文件的文件名
+//     },
+//     devtool: 'eval-source-map',
+//     devServer: {
+//         contentBase: "./public",//本地服务器所加载的页面所在的目录
+//         historyApiFallback: true,//不跳转
+//         inline: true//实时刷新
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /(\.jsx|\.js)$/,
+//                 use: {
+//                     loader: "babel-loader",
+//                     options: {
+//                         presets: [
+//                             "env", "react"
+//                         ]
+//                     }
+//                 },
+//                 exclude: /node_modules/
+//             }
+//         ]
+//     }
+// };
+
+// 安装react
+// npm install --save react react-dom
+
+
+
+// 处理css
+// npm install --save-dev style-loader css-loader
+module.exports = {
     entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
         path: __dirname + "/public",//打包后的文件存放的地方
@@ -55,10 +98,17 @@
                     }
                 },
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader"
+                    }
+                ]
             }
         ]
     }
 };
-
-// 安装react
-// npm install --save react react-dom
